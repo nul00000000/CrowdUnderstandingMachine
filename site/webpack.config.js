789df263ce51ea0c32fa8aaf7037f94b7f6bd560
nul00000000 <dev@monke.gay>
2025-11-09 23:17:13 -0500

    basic embed stuff

---
+++ site/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+const landing = {
+  entry: './src/index.ts',
+  mode: "production",
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    fallback: {
+      fs: false,
+      path: false,
+      crypto: false,
+      stream: false,
+      vm: false
+    },
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
+
+module.exports = [landing];
