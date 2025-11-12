98eacef4f133796a0a57c2af1b98340abed67953
nul00000000 <nulcreatesgames@gmail.com>
2025-11-12 05:49:36 +0000

    rooms?

--- backend/index.ts
+++ backend/index.ts
@@ -4,8 +4,46 @@ const app = express();
 
 console.log("CUM server running");
 
+app.use(express.json());
+
+type Room = {
+	votes: number,
+	lastUsed: number
+};
+
+let rooms: { [name: string]: Room; } = {};
+
+let votes = 0;
+
 app.post("/", (req, res) => {
-    console.log(req);
+	let vote = Math.sign(req.body.vote);
+	if(req.body.room in rooms) {
+		votes += vote;
+		rooms[req.body.room].votes += vote;
+		rooms[req.body.room].lastUsed = Date.now();
+		res.send({code: 0, votes: rooms[req.body.room].votes});
+	} else {
+		res.send({code: 1, error: "Room does not exist"});
+	}
+});
+
+app.post("/create/", (req, res) => {
+	if(req.body.room in rooms) {
+		res.send({code: 2, error: "Room already exists"});
+	} else {
+		rooms[req.body.room] = {votes: 0, lastUsed: Date.now()};
+		console.log(`Created room {req.body.room}`);
+		res.send({code: 0});
+	}
+});
+
+app.post("/hostinfo/", (req, res) => {
+	if(req.body.room in rooms) {
+		res.send({code: 0, votes: rooms[req.body.room].votes});
+		rooms[req.body.room].lastUsed = Date.now();
+	} else {
+		res.send({code: 1, error: "Room does not exist"});
+	}
 });
 
 app.listen(8787, () => {
