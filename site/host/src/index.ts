1f623311908822b21985f183a19e49ef7adece24
nul00000000 <dev@monke.gay>
2025-11-14 00:06:37 -0500

    wow graph and engagement

--- site/host/src/index.ts
+++ site/host/src/index.ts
@@ -1,7 +1,9 @@
 import { Chart, registerables } from "chart.js";
 Chart.register(...registerables);
 
-const counter = document.querySelector("#counter") as HTMLDivElement;
+const opinion = document.querySelector("#opinion") as HTMLDivElement;
+const engagement = document.querySelector("#engagement") as HTMLDivElement;
+const score = document.querySelector("#score") as HTMLDivElement;
 const roomCode = document.querySelector("#roomCode") as HTMLInputElement;
 const joinButton = document.querySelector("#join") as HTMLButtonElement;
 
@@ -11,11 +13,19 @@ let chart: Chart;
 
 let room: string;
 
-let roomData: Room = {votes: 0, voteGraph: [0]};
+let roomData: Room = {votes: 0, voteGraph: []};
+
+let voteGraphPoints: {x: number, y:number}[] = [];
+let engageGraphPoints: {x: number, y:number}[] = [];
+
+type VoteTick = {
+    time: number,
+    votes: number
+};
 
 type Room = {
     votes: number;
-    voteGraph: number[];
+    voteGraph: VoteTick[];
 };
 
 type HostInfo = {
@@ -41,16 +51,59 @@ function createRoom(roomCode: string) {
     req.send(JSON.stringify({room: roomCode}));
 }
 
+function loop() {
+    let req = new XMLHttpRequest();
+    req.open("POST", "/presentinator/api/hostinfo/", true);
+    req.setRequestHeader("Content-Type", "application/json");
+    req.onreadystatechange = () => {
+        if(req.readyState == 4 && req.status == 200) {
+            roomData = (JSON.parse(req.response) as HostInfo).room;
+            voteGraphPoints = roomData.voteGraph.map((val: VoteTick) => {return {x: val.time, y: val.votes}});
+            engageGraphPoints = new Array(Math.floor(roomData.voteGraph[roomData.voteGraph.length - 1].time) + 1);
+
+            let totalOpinion = 0;
+
+            for(let i = 0; i < engageGraphPoints.length - 1; i++) {
+                engageGraphPoints[i] = {x: i, y: 0};
+            }
+            engageGraphPoints[engageGraphPoints.length - 1] = {x: roomData.voteGraph[roomData.voteGraph.length - 1].time, y:0};
+            for(let i = 0; i < roomData.voteGraph.length; i++) {
+                engageGraphPoints[Math.floor(roomData.voteGraph[i].time)].y++;
+                if(i != 0) {
+                    totalOpinion += (roomData.voteGraph[i].votes + roomData.voteGraph[i - 1].votes) / 2 * (roomData.voteGraph[i].time - roomData.voteGraph[i - 1].time);
+                }
+            }
+
+            chart.data.datasets[0].data = [...voteGraphPoints];
+            chart.data.datasets[1].data = [...engageGraphPoints];
+            chart.update("none");
+
+            let opinionScore = totalOpinion / roomData.voteGraph[roomData.voteGraph.length - 1].time;
+            let engagementScore = roomData.voteGraph.length / roomData.voteGraph[roomData.voteGraph.length - 1].time;
+
+            opinion.textContent = "Opinion Score: " + Math.floor(opinionScore * 100) / 100;
+            engagement.textContent = "Engagement Score: " + Math.floor(engagementScore * 100) / 100;
+            score.textContent = "Score: " + Math.floor((opinionScore + engagementScore) * 100) / 100;
+        }
+    };
+    req.send(JSON.stringify({room: room}));
+}
+
 function setup() {
     chart = new Chart(voteGraph, {
 		type: 'line',
 		data: {
 			datasets: [{
-				label: 'Bananas Over Time',
-				data: roomData.voteGraph,
-				fill: false,
+				label: 'Opinion',
+				data: voteGraphPoints,
+				fill: true,
 				pointRadius: 0
-			}]
+			}, {
+                label: "Engagement",
+                data: [],
+                fill: true,
+                pointRadius: 0
+            }]
 		},
 		options: {
 			indexAxis: "x",
@@ -68,7 +121,7 @@ function setup() {
 					type: "linear",
 					beginAtZero: true,
 					title: {
-						text: "Bananas",
+						text: "Votes",
 						display: true
 					}
 				}
@@ -107,20 +160,7 @@ function setup() {
                 } else {
                     createRoom(room);
                 }
-                setInterval(() => {
-                    let req = new XMLHttpRequest();
-                    req.open("POST", "/presentinator/api/hostinfo/", true);
-                    req.setRequestHeader("Content-Type", "application/json");
-                    req.onreadystatechange = () => {
-                        if(req.readyState == 4 && req.status == 200) {
-                            roomData = (JSON.parse(req.response) as HostInfo).room;
-                            chart.data.datasets[0].data = [...roomData.voteGraph];
-                            chart.update("none");
-                            counter.textContent = roomData.votes + "";
-                        }
-                    };
-                    req.send(JSON.stringify({room: room}));
-                }, 100);
+                setInterval(loop, 100);
                 joinButton.style.visibility = "hidden";
                 roomCode.disabled = true;
             }
